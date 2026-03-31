fd3fd7cb14fade7642d6a45f1af178263e19204e
Rob Mellett <dev@robmellett.com>
2026-03-31 11:14:38 +0700

    Install Api

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PaymentController extends Controller{
+    public function __invoke()
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/PaymentRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PaymentRequest extends FormRequest{
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    public function authorize()
+    {
+        return true;
+    }
+}
